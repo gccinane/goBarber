c9777ab990daef4005752779bb1ddeecc4132bb2
Jose <gccinane@gmail.com>
2020-05-21 16:29:46 -0300

    db migrations e linters config

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: ['standard', 'prettier'],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIfnorePattern: 'next' }],
+  },
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router }  from 'express';
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => {
-  return res.send({mes: "DEUS"})
-})
+  return res.send({ mes: 'DEUS' });
+});
 
 export default routes;
