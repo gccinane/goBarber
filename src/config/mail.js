9084057141ace5f640dbf71a335d55936f45f25d
Jose <gccinane@gmail.com>
2020-05-28 11:05:00 -0300

    Mail management

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'c88d7d0e00b4d8',
+    pass: '3da12218301da2',
+  },
+  secure: false,
+  default: {
+    from: 'Equipe goBarber <noreply@gobarber.com>',
+  },
+};
