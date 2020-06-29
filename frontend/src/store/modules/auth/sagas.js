61dd5a2ca4aa60cf77d32c6dc1f35537a6defa97
Jose <gccinane@gmail.com>
2020-06-29 18:11:47 -0300

    loading user access

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -1,23 +1,26 @@
 import { all, takeLatest, call, put } from 'redux-saga/effects';
 import api from '~/services/api';
 import history from '~/services/history';
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'sessions', { email, password });
+    const response = yield call(api.post, 'sessions', { email, password });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('User not provider');
+    if (!user.provider) {
+      console.tron.error('User not provider');
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (error) {
+    yield put(signFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
-  console.tron.log('deus');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
