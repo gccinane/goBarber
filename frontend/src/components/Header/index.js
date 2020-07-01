155e2f21ebc704030ccb4e3f56cd7d2acf7bd911
Jose <gccinane@gmail.com>
2020-06-30 21:45:59 -0300

    notification styles

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 import { Container, Content, Profile } from './styles';
+import Notifications from '~/components/Notifications';
 
 import logo from '~/assets/logo-purple.svg';
 
@@ -15,6 +16,7 @@ function Header() {
         </nav>
 
         <aside>
+          <Notifications />
           <Profile>
             <div>
               <strong>Diego Fernandeds</strong>
