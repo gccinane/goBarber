dc72e5add7dcb210c20afaaa6837518a94c6d822
Jose <gccinane@gmail.com>
2020-08-26 18:03:20 -0300

    done button and input styles

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import Background from '~/components/Background';
 import Input from '~/components/Input';
+import Button from '~/components/Button';
 
 const SignIn = () => {
   return (
     <Background>
-      <>
-        <Text>sign up</Text>
-        <Input style={{marginTop: 38}} icon="call" placeholder="digite nome" />
-      </>
+      <Text>sign up</Text>
+      <Input style={{marginTop: 38}} icon="call" placeholder="digite nome" />
+      <Button>Entrar</Button>
     </Background>
   );
 };
