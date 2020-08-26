254491d4f0866bc5d4edda165b2a3a9a71b9ecc1
Jose <gccinane@gmail.com>
2020-08-26 18:20:36 -0300

    sign in styles done

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,15 +1,48 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Image} from 'react-native';
+
+import logo from '~/assets/logo.png';
+
 import Background from '~/components/Background';
 import Input from '~/components/Input';
 import Button from '~/components/Button';
 
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
+
 const SignIn = () => {
   return (
     <Background>
-      <Text>sign up</Text>
-      <Input style={{marginTop: 38}} icon="call" placeholder="digite nome" />
-      <Button>Entrar</Button>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite seu email"
+          />
+
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Sua senha secreta"
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 };
